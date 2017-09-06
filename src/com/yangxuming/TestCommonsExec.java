ef808a00337981c45a795766c3f13a11dfcd593f
YoungTsuiMing <yxmsspu@163.com>
2017-09-06 17:18:30 +0800

    20170906

---
+++ src/com/yangxuming/TestCommonsExec.java
@@ -0,0 +1,57 @@
+package com.yangxuming;
+
+import java.io.ByteArrayOutputStream;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+public class TestCommonsExec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String ping(String ip) {
+
+		try {
+
+			String command = "ping " + ip;
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+			ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+
+			CommandLine commandline = CommandLine.parse(command);
+
+			DefaultExecutor exec = new DefaultExecutor();
+
+			exec.setExitValues(null);
+
+			PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream, errorStream);
+
+			exec.setStreamHandler(streamHandler);
+
+			exec.execute(commandline);
+
+			String out = outputStream.toString("gbk");
+
+			String error = errorStream.toString("gbk");
+
+			return out + error;
+
+		} catch (Exception e) {
+            
+			
+			
+			
+		//	hutools.ogf.error("ping task failed.", e);
+
+			return e.toString();
+
+		}
+
+	}
+
+}
--- src/com/yangxuming/TestStr.java
+++ src/com/yangxuming/TestStr.java
@@ -1,12 +1,21 @@
 package com.yangxuming;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class TestStr {
 
+	private static Logger logger = LogManager.getLogger("TestStr");
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StringBuilder sb= new StringBuilder();
+		StringBuilder sb = new StringBuilder();
 		sb.append("www.baidu.com!");
-		sb.insert(sb.length(),"fuck you!");
-		System.out.println(sb);	
+		sb.insert(sb.length(), "fuck you!");
+		System.out.println(sb);
+
+		logger.trace("Entering application.");
+		logger.error("Didn't do it.");
+		logger.trace("Exiting application.");
+
 	}
 }
